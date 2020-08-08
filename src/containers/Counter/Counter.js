79df36d6713ec4ad8bb7297d07e06f60036cbbaa
17etro <petrpetryk2016@gmail.com>
2020-08-08 14:49:30 +0300

    Passing and Retrieving Data with Action

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -29,8 +29,8 @@ const mapDispatchToProps = dispatch => {
     return {
         onIcrementalCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD'}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT'})
+        onAddCounter: () => dispatch({type: 'ADD', val: 5}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 5})
     }
 }
 
